1fe45f97100e7dc1c902a43507b6a92a1ddff8b9
Paulo <paulo.gabriel@atlasproj.com>
2018-10-16 17:21:19 -0300

    exemplo

--- webpack.config.js
+++ webpack.config.js
@@ -13,17 +13,19 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         publicPath: '/'
     },
+    transformIgnorePatterns: "/node_modules/",
     module: {
         rules: [
             {
                 test: /\.js$/,
                 include: [path.resolve(__dirname, 'app')],
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /\.(e2e|spec)\.ts$/],
                 use: [{
                     loader: 'babel-loader',
                     options: {
                         babelrc: false,
-                        presets: ['@babel/env', "@babel/preset-react"]
+                        presets: ['@babel/env', "@babel/preset-react"],
+                        plugins: ["transform-es2015-modules-commonjs"]
                     }
                 }]
             }            
